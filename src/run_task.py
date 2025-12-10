e7c68341e5ecf89ba487d48e3e5af23d9b4e8e04
Chenyang Yang <773386@gmail.com>
2025-12-10 14:05:36 -0500

    support n>1 responses

--- src/run_task.py
+++ src/run_task.py
@@ -22,25 +22,28 @@ def run_single_instance(
         lm: dspy.LM,
         system_prompt: str,
         example: dict,
+        seed: int,
     ):
+    example = copy.deepcopy(example)
     messages = [
         {"role": "system", "content": system_prompt},
         {"role": "user", "content": example['prompt']}
     ]
-    response = lm(messages=messages)
+    response = lm(messages=messages, rollout_id=seed)
+    example["rollout_id"] = seed
     example["output"] = response[0]
-    example["outputs"] = response
-    return response
+    return example
 
 def prepare_task(
         task_id: str,
         prompt_name: str = "default",
-        max_examples: Optional[int] = None
+        max_examples: Optional[int] = None,
     ):
     assert task_id in ["webgen"], "Unsupported task_id"
 
     data_path = f"data/{task_id}/{task_id}.csv"
     data = pd.read_csv(data_path)
+    print(f"Loaded {len(data)} examples from {data_path}, keeping max_examples={max_examples}")
     data = data.to_dict(orient="records")[:max_examples]
 
     task_prompt_path = f"data/{task_id}/prompts/{prompt_name}.md"
@@ -58,16 +61,17 @@ def run_task(
         model_name: str,
         prompt_name: str = "default",
         max_examples: Optional[int] = None,
+        n_responses: int = 1,
     ):
     data, task_prompt, _ = prepare_task(task_id, max_examples=max_examples)
     lm = LM_DICT[model_name]
-    _ = batch_inference(
+    results = batch_inference(
         run_single_instance, 
-        [{"lm": lm, "system_prompt": task_prompt, "example": example} for example in data]
+        [{"lm": lm, "system_prompt": task_prompt, "example": example, "seed": seed} for example in data for seed in range(n_responses) ]
     )
     output_path = f"results/{task_id}/{model_name}_{prompt_name}.json"
     with open(output_path, "w") as f:
-        json.dump(data, f)
+        json.dump(results, f, indent=2)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run task with specified model and prompt.")
@@ -75,6 +79,7 @@ if __name__ == "__main__":
     parser.add_argument("--task_id", type=str, required=True, help="Task ID to run.")
     parser.add_argument("--prompt_name", type=str, default="default", help="Prompt name to use.")
     parser.add_argument("--max_examples", type=int, default=None, help="Maximum number of examples to process.")
+    parser.add_argument("--n_responses", type=int, default=1, help="Number of responses to generate per example.")
     args = parser.parse_args()
 
     run_task(
@@ -82,4 +87,5 @@ if __name__ == "__main__":
         model_name=args.model,
         prompt_name=args.prompt_name,
         max_examples=args.max_examples,
+        n_responses=args.n_responses,
     )
